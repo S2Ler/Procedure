d967877b66511cbecf2bebec113ca86da2a00c1a
Alexander Belyavskiy <diejmon@gmail.com>
2019-05-25 12:34:38 +0300

    Swift 5 package description

--- Sources/Procedure/Procedure.swift
+++ Sources/Procedure/Procedure.swift
@@ -1,100 +1,100 @@
 import Foundation
 
 public final class Procedure<Input, Output>: ConcurrentOperation {
-    private var input: Input!
-    private var output: Output!
+  private var input: Input!
+  private var output: Output!
 
-    private let work: (_ input: Input, _ fullfill: @escaping (Output) -> Void) -> Void
-    private let queue: OperationQueue
+  private let work: (_ input: Input, _ fullfill: @escaping (Output) -> Void) -> Void
+  private let queue: OperationQueue
 
-    public init(executeOn queue: OperationQueue = .main,
-                _ work: @escaping (_ input: Input, _ fullfill: @escaping (Output) -> Void) -> Void) {
-        self.work = work
-        self.queue = queue
+  public init(executeOn queue: OperationQueue = .main,
+              _ work: @escaping (_ input: Input, _ fullfill: @escaping (Output) -> Void) -> Void) {
+    self.work = work
+    self.queue = queue
+  }
+
+  public convenience init(executeOn queue: OperationQueue = .main, _ work: @escaping (Input) -> Output) {
+    self.init(executeOn: queue) { input, fullfill in
+      fullfill(work(input))
+    }
+  }
+
+  public override func execute() {
+    if Input.self == Void.self {
+      runVoidMain()
+    } else {
+      runMain()
+    }
+  }
+
+  public func then<NewOutput>(_ nextProcedure: Procedure<Output, NewOutput>) -> Procedure<Output, NewOutput> {
+    guard !isFinished else {
+      nextProcedure.input = output
+      nextProcedure.queue.addOperation(nextProcedure)
+      return nextProcedure
     }
 
-    public convenience init(executeOn queue: OperationQueue = .main, _ work: @escaping (Input) -> Output) {
-        self.init(executeOn: queue) { input, fullfill in
-            fullfill(work(input))
-        }
+    let adapter = BlockOperation { [unowned nextProcedure] in
+      nextProcedure.input = self.output
     }
 
-    public override func execute() {
-        if Input.self == Void.self {
-            runVoidMain()
-        } else {
-            runMain()
-        }
+    adapter.addDependency(self)
+    nextProcedure.addDependency(adapter)
+
+    if !isAlreadyAddedToQueue() {
+      addToQueue()
     }
+    queue.addOperation(adapter)
 
-    public func then<NewOutput>(_ nextProcedure: Procedure<Output, NewOutput>) -> Procedure<Output, NewOutput> {
-        guard !isFinished else {
-            nextProcedure.input = output
-            nextProcedure.queue.addOperation(nextProcedure)
-            return nextProcedure
-        }
-
-        let adapter = BlockOperation { [unowned nextProcedure] in
-            nextProcedure.input = self.output
-        }
-
-        adapter.addDependency(self)
-        nextProcedure.addDependency(adapter)
-
-        if !isAlreadyAddedToQueue() {
-            addToQueue()
-        }
-        queue.addOperation(adapter)
-
-        guard !nextProcedure.isAlreadyAddedToQueue() else {
-            preconditionFailure("Two procedures chains to the same procedure isn't supported.")
-        }
-        nextProcedure.addToQueue()
-
-        return nextProcedure
+    guard !nextProcedure.isAlreadyAddedToQueue() else {
+      preconditionFailure("Two procedures chains to the same procedure isn't supported.")
     }
+    nextProcedure.addToQueue()
 
-    public func finally(_ block: @escaping (Output) -> Void) {
-        let finalBlock = Procedure<Output, Void> { output, fullfill in
-            block(output)
-            fullfill(())
-        }
-        _ = then(finalBlock)
-    }
+    return nextProcedure
+  }
 
-    public func then<NewOutput>(executeOn queue: OperationQueue,
-                                _ work: @escaping (Output) -> NewOutput) -> Procedure<Output, NewOutput> {
-        return then(Procedure<Output, NewOutput>(executeOn: queue, work))
+  public func finally(_ block: @escaping (Output) -> Void) {
+    let finalBlock = Procedure<Output, Void> { output, fullfill in
+      block(output)
+      fullfill(())
     }
+    _ = then(finalBlock)
+  }
 
-    /// Returns new procedure which will perform work on the same queue as this procedure with results of this procedure
-    public func then<NewOutput>(_ work: @escaping (Output) -> NewOutput) -> Procedure<Output, NewOutput> {
-        return then(Procedure<Output, NewOutput>(executeOn: queue, work))
-    }
+  public func then<NewOutput>(executeOn queue: OperationQueue,
+                              _ work: @escaping (Output) -> NewOutput) -> Procedure<Output, NewOutput> {
+    return then(Procedure<Output, NewOutput>(executeOn: queue, work))
+  }
+
+  /// Returns new procedure which will perform work on the same queue as this procedure with results of this procedure
+  public func then<NewOutput>(_ work: @escaping (Output) -> NewOutput) -> Procedure<Output, NewOutput> {
+    return then(Procedure<Output, NewOutput>(executeOn: queue, work))
+  }
 }
 
 private extension Procedure {
-    func runMain() {
-        work(input) { [unowned self] output in
-            self.output = output
-            self.finish()
-        }
+  func runMain() {
+    work(input) { [unowned self] output in
+      self.output = output
+      self.finish()
     }
+  }
 
-    private func runVoidMain() {
-        work(() as! Input) { [unowned self] output in
-            self.output = output
-            self.finish()
-        }
+  private func runVoidMain() {
+    work(() as! Input) { [unowned self] output in
+      self.output = output
+      self.finish()
     }
+  }
 
-    private func addToQueue() {
-        queue.addOperation(self)
-    }
+  private func addToQueue() {
+    queue.addOperation(self)
+  }
 
-    private func isAlreadyAddedToQueue() -> Bool {
-        return queue.operations.contains(self)
-    }
+  private func isAlreadyAddedToQueue() -> Bool {
+    return queue.operations.contains(self)
+  }
 }
 
 /// An abstract class that makes building simple asynchronous operations easy.
@@ -102,95 +102,95 @@ private extension Procedure {
 /// `finish()` when they are done. All `NSOperation` work will be handled
 /// automatically.
 open class ConcurrentOperation: Operation {
-    @objc(DSLConcurrentOperationState)
-    private enum State: Int {
-        case ready
-        case executing
-        case finished
+  @objc(DSLConcurrentOperationState)
+  private enum State: Int {
+    case ready
+    case executing
+    case finished
+  }
+
+  private let stateQueue = DispatchQueue(label: "com.diejmon.operation.state", attributes: .concurrent)
+
+  private var rawState = State.ready
+
+  @objc
+  private dynamic var state: State {
+    get {
+      return stateQueue.sync { rawState }
+    }
+    set {
+      willChangeValue(forKey: #keyPath(state))
+      stateQueue.sync(flags: .barrier) { rawState = newValue }
+      didChangeValue(forKey: #keyPath(state))
+    }
+  }
+
+  public final override var isReady: Bool {
+    return state == .ready && super.isReady
+  }
+
+  public final override var isExecuting: Bool {
+    return state == .executing
+  }
+
+  public final override var isFinished: Bool {
+    return state == .finished
+  }
+
+  // MARK: - NSObject
+
+  @objc
+  private dynamic class func keyPathsForValuesAffectingIsReady() -> Set<String> {
+    return [#keyPath(state)]
+  }
+
+  @objc
+  private dynamic class func keyPathsForValuesAffectingIsExecuting() -> Set<String> {
+    return [#keyPath(state)]
+  }
+
+  @objc private dynamic class func keyPathsForValuesAffectingIsFinished() -> Set<String> {
+    return [#keyPath(state)]
+  }
+
+  // MARK: - Foundation.Operation
+
+  public final override func start() {
+    super.start()
+
+    guard !isCancelled else {
+      finish()
+      return
     }
 
-    private let stateQueue = DispatchQueue(label: "com.diejmon.operation.state", attributes: .concurrent)
+    state = .executing
+    execute()
+  }
 
-    private var rawState = State.ready
+  // MARK: - Public
 
-    @objc
-    private dynamic var state: State {
-        get {
-            return stateQueue.sync { rawState }
-        }
-        set {
-            willChangeValue(forKey: #keyPath(state))
-            stateQueue.sync(flags: .barrier) { rawState = newValue }
-            didChangeValue(forKey: #keyPath(state))
-        }
-    }
+  /// Subclasses must implement this to perform their work and they must not
+  /// call `super`. The default implementation of this function throws an
+  /// exception.
+  open func execute() {
+    fatalError("Subclasses must implement `execute`.")
+  }
 
-    public final override var isReady: Bool {
-        return state == .ready && super.isReady
-    }
-
-    public final override var isExecuting: Bool {
-        return state == .executing
-    }
-
-    public final override var isFinished: Bool {
-        return state == .finished
-    }
-
-    // MARK: - NSObject
-
-    @objc
-    private dynamic class func keyPathsForValuesAffectingIsReady() -> Set<String> {
-        return [#keyPath(state)]
-    }
-
-    @objc
-    private dynamic class func keyPathsForValuesAffectingIsExecuting() -> Set<String> {
-        return [#keyPath(state)]
-    }
-
-    @objc private dynamic class func keyPathsForValuesAffectingIsFinished() -> Set<String> {
-        return [#keyPath(state)]
-    }
-
-    // MARK: - Foundation.Operation
-
-    public final override func start() {
-        super.start()
-
-        guard !isCancelled else {
-            finish()
-            return
-        }
-
-        state = .executing
-        execute()
-    }
-
-    // MARK: - Public
-
-    /// Subclasses must implement this to perform their work and they must not
-    /// call `super`. The default implementation of this function throws an
-    /// exception.
-    open func execute() {
-        fatalError("Subclasses must implement `execute`.")
-    }
-
-    /// Call this function after any work is done or after a call to `cancel()`
-    /// to move the operation into a completed state.
-    public final func finish() {
-        state = .finished
-    }
+  /// Call this function after any work is done or after a call to `cancel()`
+  /// to move the operation into a completed state.
+  public final func finish() {
+    state = .finished
+  }
 }
 
 public extension Operation {
-    func run(on queue: OperationQueue) {
-        queue.addOperation(self)
-    }
+  func run(on queue: OperationQueue) {
+    queue.addOperation(self)
+  }
 
-    func addDependencies(_ operations: [Operation]) {
-        operations.forEach {
-            addDependency($0)
-        }
+  func addDependencies(_ operations: [Operation]) {
+    operations.forEach {
+      addDependency($0)
     }
+  }
 }
